8568b47943804cbddfea8cf3fc429c01c5e14a4e
imuhammadjabbar@gmail.com <pmsabertlili@gmail.com>
2022-06-26 17:11:18 +0500

    Disco Invest Demo

---
+++ Disco Invest/Modules/Cotations/ViewController/CotationsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CotationsViewController.swift
+//  Disco Invest
+//
+//  Created by Jabbar on 26/06/2022.
+//
+
+import UIKit
+
+class CotationsViewController: UIViewController {
+
+    //
+    // MARK: - IBOutlets
+    //
+    @IBOutlet weak var btnCotations: UIButton!
+    
+    //
+    // MARK: - View LifeCycle
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //
+    // MARK: - 
+    //
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Disco InvestUITests/Disco_InvestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Disco_InvestUITestsLaunchTests.swift
+//  Disco InvestUITests
+//
+//  Created by Jabbar on 26/06/2022.
+//
+
+import XCTest
+
+class Disco_InvestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
